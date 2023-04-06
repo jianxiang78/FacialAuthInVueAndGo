6fce5dc6ad24676c20043af8e4d3e5a1a9230912
jianxiang78 <jianxiang78@qq.com>
2023-04-07 00:18:27 +0800

    init

--- back-end/src/handlers/config.go
+++ back-end/src/handlers/config.go
@@ -13,14 +13,10 @@ func Config(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	result := make(map[string]string)
 	result["FaceIOAppCode"] = conf.GetEnv().FaceIOAppCode
-
 	jsonResult := new(model.JsonResult)
 	jsonResult.Code = conf.GetEnv().CodeSuccess
 	jsonResult.Msg = conf.GetEnv().MsgSuccess
 	jsonResult.Result = result
-
 	msg, _ := json.Marshal(jsonResult)
-
 	_, _ = io.WriteString(w, string(msg))
-
 }
